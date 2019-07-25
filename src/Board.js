f39f3a568639f3047406cd410b8e1a5637fac05a
jcraig1730 <jcraig1730@gmail.com>
2019-07-24 20:55:25 -0700

    Complete board.js and start solver.js

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,29 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      let problem;
+      let rowOccupied = false;
+      this.rows()[rowIndex].forEach((col) => {
+        if (rowOccupied && col) {
+          problem = true;
+        }
+        if (!rowOccupied && col) {
+          rowOccupied = true;
+        }
+      })
+      return problem || false; // fixme
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      let problem
+      this.rows().forEach((row, rowIdx) => {
+        if (this.hasRowConflictAt(rowIdx)) {
+          problem = true;
+        }
+      });
+
+      return problem || false; // fixme
     },
 
 
@@ -94,12 +111,32 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      let problem;
+      let colOccupied = false;
+      this.rows().forEach(row => {
+        if (colOccupied && row[colIndex]) {
+          problem = true;
+        }
+        if (!colOccupied && row[colIndex]) {
+          colOccupied = true;
+        }
+      })
+      return problem || false; // fixme
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      let problem;
+      let occupiedCols = []
+      this.rows().forEach(row => {
+
+        row.forEach((col, colIdx) => {
+          if (this.hasColConflictAt(colIdx)) {
+            problem = true;
+          }
+        })
+      })
+      return problem || false; // fixme
     },
 
 
@@ -109,12 +146,25 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var index = majorDiagonalColumnIndexAtFirstRow;
+      var count = 0;
+      for (let row = 0; row < this.rows().length; row++){
+        if (this.rows()[row][index]){
+          count++
+        }
+        index++
+      }
+      return count > 1 ? true : false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      for (let col = -(this.rows()[0].length - 1); col < this.rows()[0].length; col++){
+        if (this.hasMajorDiagonalConflictAt(col)) {
+          return true;
+        }
+      }
+      return false
     },
 
 
@@ -124,12 +174,25 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var index = minorDiagonalColumnIndexAtFirstRow;
+      var count = 0;
+      for (let row = 0; row < this.rows().length; row++){
+        if (this.rows()[row][index]) {
+          count++
+        }
+        index--;
+      }
+      return count > 1 ? true : false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      for (let col = 0; col < 2*(this.rows()[0].length) -1; col++){
+        if (this.hasMinorDiagonalConflictAt(col)) {
+          return true;
+        }
+      }
+      return false
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,17 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = []; //fixme
+  for (var i = 0; i < n; i++){
+    solution.push([].fill(0, 0, n))
+  }
+  console.log(solution)
+  for (var row = 0; row < n; row++){
+    for (var col =0; col < n; col++){
+
+    }
+  }
+
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -24,7 +34,14 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+
+  var inner = function (n){
+    if (n === 1) {
+      return 1;
+    }
+    return  n * inner(n-1);
+  }
+  var solutionCount = inner(n);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -40,7 +57,17 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount; //fixme
+  if (n === 0 || n === 1) {
+    solutionCount = 1;
+  } else if (n === 3 || n === 2) {
+    solutionCount = 0;
+  } else if (n % 2 === 0) {
+    solutionCount = n - 2;
+  } else {
+    solutionCount = 2 * n;
+  }
+  console.log(solutionCount)
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
